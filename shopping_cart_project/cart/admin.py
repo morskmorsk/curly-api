8162c11c6d0663f0e712deecfd0217aea3fe2765
morsk <khalidfour@yahoo.com>
2024-07-13 07:58:53 +0100

    chore: Update admin.py and models.py for cart app

--- shopping_cart_project/cart/admin.py
+++ shopping_cart_project/cart/admin.py
@@ -1,7 +1,7 @@
 # cart/admin.py
 
 from django.contrib import admin
-from .models import Product, CartItem, Location, Department
+from .models import Product, CartItem, Location, Department, Cart, CartItem, Order, OrderItem
 
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
@@ -24,3 +24,21 @@ class ProductAdmin(admin.ModelAdmin):
 class CartItemAdmin(admin.ModelAdmin):
     list_display = ('product', 'quantity', 'subtotal', 'tax', 'total_price', 'created_at', 'updated_at')
     list_filter = ('product__department', 'product__location', 'product__department__is_taxable')
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'total', 'created_at', 'updated_at')
+    list_filter = ('user',)
+    search_fields = ('user',)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'total', 'created_at', 'updated_at')
+    list_filter = ('user',)
+    search_fields = ('user',)
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('product', 'quantity')
+    list_filter = ('product__department', 'product__location', 'product__department__is_taxable')
+    search_fields = ('product', 'quantity', 'subtotal', 'tax', 'total_price')
